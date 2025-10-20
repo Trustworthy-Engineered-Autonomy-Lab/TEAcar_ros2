0a4cf922646dcf9f883feaa9703f383165b31f31
Difei Chen <chen.difei@ufl.edu>
2025-10-20 19:39:51 -0400

    Migrate nn_controller to ROS2 with working image inference and steer output

---
+++ src/nn_controller/plugins/dummy_inferencer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class DummyInferencer:
+    def __init__(self, logger):
+        self.logger = logger
+        self.k = 0.002  # gain
+
+    def infer(self, bgr_image):
+        mean_val = float(np.mean(bgr_image))
+        steer = self.k * (mean_val - 127.0)
+        return max(-1.0, min(1.0, steer))
